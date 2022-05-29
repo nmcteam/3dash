59e98693ee6b04f319ebed770d6544f120b2816c
Josh Lockhart <josh@newmediacampaigns.com>
2022-05-29 16:58:52 +0000

    Demo collections

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require './vendor/autoload.php';
 
 use Nmc\Ssg\App;
+use Nmc\Ssg\Collections;
 use Nmc\Ssg\Drafts;
 use Nmc\Ssg\FilesystemWriter;
 use Nmc\Ssg\HeaderIni;
@@ -12,6 +13,14 @@ $app = new App('./site');
 $app->add(new HeaderIni());
 $app->add(new Drafts());
 $app->add(new PublishDate('date'));
+$app->add(new Collections([
+    'recent_news' => [
+        'pattern' => '#^/news/.*#',
+        'sortBy' => 'date',
+        'reverse' => true,
+        'limit' => 2
+    ]
+]));
 $app->add(new Twig());
 $app->add(new FilesystemWriter('./build'));
 $app->run();
